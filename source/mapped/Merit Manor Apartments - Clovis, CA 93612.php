b84cc3f9bde13523e1f5807880b6747acb9570a5
mialeevang <104871378+mialeevang@users.noreply.github.com>
2024-06-04 19:59:13 -0700

    Add files via upload

---
+++ source/mapped/Merit Manor Apartments - Clovis, CA 93612.php
@@ -0,0 +1,169 @@
+<div class="mapp-content">
+
+<span class="mapp-title">
+	Merit Manor Apartments
+	<span class="mapp-city">-
+	Clovis, CA 93612
+	</span>
+</span>
+
+<ul class="mapp-list">
+
+<li class="mapp-table">
+<span class="mapp-address">400 W Gettysburg Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">office</td>
+<td class="coord">36.800408419238195, -119.71925224530843</td>
+</tr>
+
+<tr>
+<td class="unit">101A 102A 103A 104A 201A 202A 203A 204A</td>
+<td class="coord">36.80099797449012, -119.71919428693373</td>
+</tr>
+
+<tr>
+<td class="unit">105A 106A 107A 205A 206A 207A</td>
+<td class="coord">36.80097047361905, -119.71886342507472</td>
+</tr>
+
+<tr>
+<td class="unit">108A 109A 110A 111A 208A 209A 210A 211A</td>
+<td class="coord">36.80098164380268, -119.71856839593536</td>
+</tr>
+
+<tr>
+<td class="unit">112A 113A 114A 115A 212A 213A 214A 215A</td>
+<td class="coord">36.800755939384715, -119.718498261348</td>
+</tr>
+
+<tr>
+<td class="unit">116A 117A 118A 119A 120A 121A 216A 217A 218A 219A 220A 221A</td>
+<td class="coord">36.800751763368964, -119.7188451153547</td>
+</tr>
+
+<tr>
+<td class="unit">122A 123A 124A 222A 223A 224A</td>
+<td class="coord">36.800724844825474, -119.71915547782164</td>
+</tr>
+
+<tr>
+<td class="unit">125A 126A 127A 128A 129A 130A 225A 226A 227A 228A 229A 230A</td>
+<td class="coord">36.80034028940222, -119.71907102065738</td>
+</tr>
+
+<tr>
+<td class="unit">131A 132A 133A 134A 231A 232A 233A 234A</td>
+<td class="coord">36.80019555446267, -119.71879091386604</td>
+</tr>
+
+<tr>
+<td class="unit">135A 136A 137A 138A 235A 236A 237A 238A</td>
+<td class="coord">36.8002144038152, -119.71847682643502</td>
+</tr>
+
+<tr>
+<td class="unit">139A 140A 141A 142A 143A 144A 239A 240A 241A 242A 243A 244A</td>
+<td class="coord">36.79986466374999, -119.71848034804437</td>
+</tr>
+
+<tr>
+<td class="unit">145A 146A 147A 245A 246A 247A</td>
+<td class="coord">36.79968874455971, -119.71875489043602</td>
+</tr>
+
+<tr>
+<td class="unit">148A 149A 150A 151A 152A 153A 248A 249A 250A 251A 252A 253A</td>
+<td class="coord">36.79980134693682, -119.71899610089389</td>
+</tr>
+
+<tr>
+<td class="unit">154A 155A 156A 157A 254A 255A 256A 257A</td>
+<td class="coord">36.799938537450856, -119.71875548574049</td>
+</tr>
+
+<tr>
+<td class="unit">158A 159A 160A 161A 162A 258A 259A 260A 261A 262A</td>
+<td class="coord">36.80006019439781, -119.71916833273615</td>
+</tr>
+
+<tr>
+<td class="unit">101B 102B 201B 202B</td>
+<td class="coord">36.80101202857575, -119.71957645573245</td>
+</tr>
+
+<tr>
+<td class="unit">103B 104B 105B 106B 107B 108B 203B 204B 205B 206B 207B 208B</td>
+<td class="coord">36.80099180135143, -119.71994792938774</td>
+</tr>
+
+<tr>
+<td class="unit">109B 110B 111B 209B 210B 211B</td>
+<td class="coord">36.80080792962084, -119.72013363474014</td>
+</tr>
+
+<tr>
+<td class="unit">112B 113B 114B 115B 116B 212B 213B 214B 215B 216B</td>
+<td class="coord">36.80075995694985, -119.71977937675763</td>
+</tr>
+
+<tr>
+<td class="unit">117B 118B 119B 120B 121B 122B 217B 218B 219B 220B 221B 222B</td>
+<td class="coord">36.8007241960986, -119.71945036269045</td>
+</tr>
+
+<tr>
+<td class="unit">123B 124B 125B 126B 127B 128B 223B 224B 225B 226B 227B 228B</td>
+<td class="coord">36.800363551926075, -119.719531447052</td>
+</tr>
+
+<tr>
+<td class="unit">129B 130B 131B 132B 229B 230B 231B 232B</td>
+<td class="coord">36.80023308756023, -119.71983245738213</td>
+</tr>
+
+<tr>
+<td class="unit">133B 134B 135B 136B 137B 233B 234B 235B 236B 237B</td>
+<td class="coord">36.80015630164897, -119.72013542337899</td>
+</tr>
+
+<tr>
+<td class="unit">138B 139B 140B 141B 142B 143B 238B 239B 240B 241B 242B 243B</td>
+<td class="coord">36.79980271939684, -119.72012531821113</td>
+</tr>
+
+<tr>
+<td class="unit">144B 145B 244B 245B</td>
+<td class="coord">36.799682224377264, -119.71989597345905</td>
+</tr>
+
+<tr>
+<td class="unit">146B 147B 148B 149B 246B 247B 248B 249B</td>
+<td class="coord">36.79995650628336, -119.71988302377484</td>
+</tr>
+
+<tr>
+<td class="unit">150B 151B 152B 153B 250B 251B 252B 253B</td>
+<td class="coord">36.80006145663881, -119.71955141822883</td>
+</tr>
+
+<tr>
+<td class="unit">154B 155B 156B 157B 158B 159B 254B 255B 256B 257B 258B 259B</td>
+<td class="coord">36.79980106557995, -119.71965152581194</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+</ul>
+</div>
---
+++ source/mapped/Pacific Grove Apartments - Clovis, CA 93612.php
@@ -0,0 +1,387 @@
+<div class="mapp-content">
+
+<span class="mapp-title">
+	Pacific Grove Apartments
+	<span class="mapp-city">-
+	Clovis, CA 93612
+	</span>
+</span>
+
+<ul class="mapp-list">
+
+<li class="mapp-table">
+<span class="mapp-address">287 W Barstow Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">101 102 103 104</td>
+<td class="coord">36.81598394591462, -119.71562320320628</td>
+</tr>
+
+<tr>
+<td class="unit">105 106 107 108</td>
+<td class="coord">36.81597951257063, -119.71525281755879</td>
+</tr>
+
+<tr>
+<td class="unit">109 110 111 112</td>
+<td class="coord">36.8160357067094, -119.71502138618939</td>
+</tr>
+
+<tr>
+<td class="unit">113 114 115 116</td>
+<td class="coord">36.816239673935065, -119.71507183404923</td>
+</tr>
+
+<tr>
+<td class="unit">117 118 119 120</td>
+<td class="coord">36.8162169570692, -119.71533646816316</td>
+</tr>
+
+<tr>
+<td class="unit">121 122 123 124</td>
+<td class="coord">36.8164839862787, -119.71509799694334</td>
+</tr>
+
+<tr>
+<td class="unit">125 126 127 128</td>
+<td class="coord">36.816813422079385, -119.71503381496223</td>
+</tr>
+
+<tr>
+<td class="unit">129 130 131 132</td>
+<td class="coord">36.816872505046426, -119.71538431248621</td>
+</tr>
+
+<tr>
+<td class="unit">133 134 135 136</td>
+<td class="coord">36.81691563865797, -119.7156820598482</td>
+</tr>
+
+<tr>
+<td class="unit">137 138 139 140</td>
+<td class="coord">36.816920388194674, -119.71596021763436</td>
+</tr>
+
+<tr>
+<td class="unit">141 142 143 144</td>
+<td class="coord">36.81668796657341, -119.71572082053447</td>
+</tr>
+
+<tr>
+<td class="unit">145 146 147 148</td>
+<td class="coord">36.81664356009606, -119.71543173982244</td>
+</tr>
+
+<tr>
+<td class="unit">149 150 151 152</td>
+<td class="coord">36.81646171260388, -119.71580716684042</td>
+</tr>
+
+<tr>
+<td class="unit">153 154 155 156</td>
+<td class="coord">36.81645329211346, -119.7155047629579</td>
+</tr>
+
+<tr>
+<td class="unit">157 158 159 160</td>
+<td class="coord">36.81622692052335, -119.7157989370104</td>
+</tr>
+
+<tr>
+<td class="unit">161 162 163 164</td>
+<td class="coord">36.81623150241937, -119.71553464933663</td>
+</tr>
+
+<tr>
+<td class="unit">165 166 167 168</td>
+<td class="coord">36.81602048308381, -119.71584772925847</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">359 W Barstow Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">pool</td>
+<td class="coord">36.816420462472564, -119.71715304151574</td>
+</tr>
+
+<tr>
+<td class="unit">101 102 103 104</td>
+<td class="coord">36.81601335434369, -119.71697306491426</td>
+</tr>
+
+<tr>
+<td class="unit">105 106 107 108</td>
+<td class="coord">36.81597101757849, -119.71674071195365</td>
+</tr>
+
+<tr>
+<td class="unit">109 110 111 112</td>
+<td class="coord">36.81600136682767, -119.71651435213222</td>
+</tr>
+
+<tr>
+<td class="unit">113 114 115 116</td>
+<td class="coord">36.81618424436906, -119.71656719128522</td>
+</tr>
+
+<tr>
+<td class="unit">117 118 119 120</td>
+<td class="coord">36.81638033033698, -119.7165642975341</td>
+</tr>
+
+<tr>
+<td class="unit">121 122 123 124</td>
+<td class="coord">36.81660009185308, -119.71656085048863</td>
+</tr>
+
+<tr>
+<td class="unit">125 126 127 128</td>
+<td class="coord">36.81642186512812, -119.71684341719549</td>
+</tr>
+
+<tr>
+<td class="unit">129 130 131 132</td>
+<td class="coord">36.81623715200291, -119.71685076468127</td>
+</tr>
+
+<tr>
+<td class="unit">133 134 135 136</td>
+<td class="coord">36.81626580459307, -119.71710583416034</td>
+</tr>
+
+<tr>
+<td class="unit">137 138 139 140</td>
+<td class="coord">36.816285433444506, -119.71741070004971</td>
+</tr>
+
+<tr>
+<td class="unit">141 142 143 144</td>
+<td class="coord">36.816125096878764, -119.71746361633522</td>
+</tr>
+
+<tr>
+<td class="unit">145 146 147 148</td>
+<td class="coord">36.815963919350864, -119.71745509978253</td>
+</tr>
+
+<tr>
+<td class="unit">149 150 151 152</td>
+<td class="coord">36.81601084410832, -119.71722262771216</td>
+</tr>
+
+<tr>
+<td class="unit">153 154 155 156</td>
+<td class="coord">36.81649261538336, -119.71758562954008</td>
+</tr>
+
+<tr>
+<td class="unit">157 158 159 160</td>
+<td class="coord">36.816580209689, -119.71731020441617</td>
+</tr>
+
+<tr>
+<td class="unit">161 162 163 164</td>
+<td class="coord">36.81668474230895, -119.71708807486645</td>
+</tr>
+
+<tr>
+<td class="unit">165 166 167 168</td>
+<td class="coord">36.81680712311218, -119.71684635112666</td>
+</tr>
+
+<tr>
+<td class="unit">169 170 171 172</td>
+<td class="coord">36.81690672986774, -119.71657100448425</td>
+</tr>
+
+<tr>
+<td class="unit">173 174 175 176</td>
+<td class="coord">36.8170397810523, -119.71632420507906</td>
+</tr>
+
+<tr>
+<td class="unit">177 178 179 180</td>
+<td class="coord">36.81720057511923, -119.7160641799658</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">1064 Villa Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">101 102 103 104</td>
+<td class="coord">36.81741271939374, -119.71382266277062</td>
+</tr>
+
+<tr>
+<td class="unit">105 106 107 108</td>
+<td class="coord">36.81741931341876, -119.7141245244239</td>
+</tr>
+
+<tr>
+<td class="unit">109 110 111 112</td>
+<td class="coord">36.817418850278194, -119.71439867249622</td>
+</tr>
+
+<tr>
+<td class="unit">113 114 115 116</td>
+<td class="coord">36.81743679377253, -119.71465722132234</td>
+</tr>
+
+<tr>
+<td class="unit">117 118 119 120</td>
+<td class="coord">36.817456714714915, -119.71495552595049</td>
+</tr>
+
+<tr>
+<td class="unit">121 122 123 124</td>
+<td class="coord">36.817447201898744, -119.71525951473676</td>
+</tr>
+
+<tr>
+<td class="unit">125 126 127 128</td>
+<td class="coord">36.81743801050735, -119.71552952165823</td>
+</tr>
+
+<tr>
+<td class="unit">129 130 131 132</td>
+<td class="coord">36.817329625488064, -119.7158135543812</td>
+</tr>
+
+<tr>
+<td class="unit">133 134 135 136</td>
+<td class="coord">36.817188267395224, -119.71415589136733</td>
+</tr>
+
+<tr>
+<td class="unit">137 138 139 140</td>
+<td class="coord">36.81717363458203, -119.7138479783045</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">1108 Villa Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">pool</td>
+<td class="coord">36.8164771555299, -119.71438309907937</td>
+</tr>
+
+<tr>
+<td class="unit">101 102 103 104</td>
+<td class="coord">36.81651367451054, -119.71382764022768</td>
+</tr>
+
+<tr>
+<td class="unit">105 106 107 108 office</td>
+<td class="coord">36.81669671838991, -119.7138589503458</td>
+</tr>
+
+<tr>
+<td class="unit">109 110 111 112</td>
+<td class="coord">36.81642377759276, -119.71408390618284</td>
+</tr>
+
+<tr>
+<td class="unit">113 114 115 116</td>
+<td class="coord">36.8166587720369, -119.71417197217612</td>
+</tr>
+
+<tr>
+<td class="unit">117 118 119 120</td>
+<td class="coord">36.816691821730565, -119.71447116867786</td>
+</tr>
+
+<tr>
+<td class="unit">121 122 123 124</td>
+<td class="coord">36.81672026303858, -119.71477038875234</td>
+</tr>
+
+<tr>
+<td class="unit">125 126 127 128</td>
+<td class="coord">36.81650615124572, -119.71477008911543</td>
+</tr>
+
+<tr>
+<td class="unit">129 130 131 132</td>
+<td class="coord">36.81627387080838, -119.71435641673004</td>
+</tr>
+
+<tr>
+<td class="unit">133 134 135 136</td>
+<td class="coord">36.81603402067395, -119.71435280952895</td>
+</tr>
+
+<tr>
+<td class="unit">137 138 139 140</td>
+<td class="coord">36.8160130935641, -119.71416544902749</td>
+</tr>
+
+<tr>
+<td class="unit">141 142 143 144</td>
+<td class="coord">36.81599048461482, -119.71390138512999</td>
+</tr>
+
+<tr>
+<td class="unit">145 146 147 148</td>
+<td class="coord">36.81620394496114, -119.71382890318624</td>
+</tr>
+
+<tr>
+<td class="unit">149 150 151 152</td>
+<td class="coord">36.816228288713916, -119.71407830537422</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+</ul>
+</div>
