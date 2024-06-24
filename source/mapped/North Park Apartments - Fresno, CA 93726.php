0d72dca2356441b4c444e9692050703a86ecf3ec
mialeevang <104871378+mialeevang@users.noreply.github.com>
2024-06-24 11:09:42 -0700

    Add files via upload

---
+++ source/mapped/North Park Apartments - Fresno, CA 93726.php
@@ -0,0 +1,754 @@
+<div class="mapp-content">
+
+<span class="mapp-title">
+	North Park Apartments
+	<span class="mapp-city">-
+	Fresno, CA 93726
+	</span>
+</span>
+
+<?php echo 
+'<ul class="mapp-note">', 
+
+$dblchkad,
+
+'</ul>'; 
+?>
+
+<ul class="mapp-list">
+
+<li class="mapp-table">
+<span class="mapp-address">4890 N Backer Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="unit">office</td>
+<td class="coord">36.80638482086142, -119.74255358242856</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4669 E Alamos Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">4</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.80577283857832, -119.74229491175419</td>
+</tr>
+
+<tr>
+<td class="bldg">4</td>
+<td class="unit">103 104 203 204</td>
+<td class="coord">36.805894759314725, -119.74229902725388</td>
+</tr>
+
+<tr>
+<td class="bldg">3</td>
+<td class="unit">105 106 205 206</td>
+<td class="coord">36.80584156857829, -119.7426132234643</td>
+</tr>
+
+<tr>
+<td class="bldg">3</td>
+<td class="unit">107 108 207 208</td>
+<td class="coord">36.805899272009746, -119.74278464711047</td>
+</tr>
+
+<tr>
+<td class="bldg">2</td>
+<td class="unit">109 110 209 210</td>
+<td class="coord">36.805962200650065, -119.7428944611046</td>
+</tr>
+
+<tr>
+<td class="bldg">2</td>
+<td class="unit">111 112 211 212</td>
+<td class="coord">36.80609554795982, -119.74297750044194</td>
+</tr>
+
+<tr>
+<td class="bldg">2</td>
+<td class="unit">113 213</td>
+<td class="coord">36.806227032415684, -119.74303743875222</td>
+</tr>
+
+<tr>
+<td class="bldg">1</td>
+<td class="unit">114 115</td>
+<td class="coord">36.80628811228715, -119.74306461490588</td>
+</tr>
+
+<tr>
+<td class="bldg">1</td>
+<td class="unit">116 117</td>
+<td class="coord">36.806291453216716, -119.74295335209267</td>
+</tr>
+
+<tr>
+<td class="bldg">1</td>
+<td class="unit">118 119</td>
+<td class="coord">36.80624860292277, -119.7428421940044</td>
+</tr>
+
+<tr>
+<td class="bldg">1</td>
+<td class="unit">120 121</td>
+<td class="coord">36.806244935171165, -119.74273321350609</td>
+</tr>
+
+<tr>
+<td class="bldg">5</td>
+<td class="unit">122 123</td>
+<td class="coord">36.80616310810009, -119.742284315335</td>
+</tr>
+
+<tr>
+<td class="bldg">5</td>
+<td class="unit">124 125</td>
+<td class="coord">36.80615568155546, -119.74217272911537</td>
+</tr>
+
+<tr>
+<td class="bldg">6</td>
+<td class="unit">126 127</td>
+<td class="coord">36.80618121558899, -119.74202398602996</td>
+</tr>
+
+<tr>
+<td class="bldg">6</td>
+<td class="unit">128 129</td>
+<td class="coord">36.80618493722912, -119.7419168685944</td>
+</tr>
+
+<tr>
+<td class="bldg">7</td>
+<td class="unit">130 131 230 231</td>
+<td class="coord">36.80606781987114, -119.74177249785922</td>
+</tr>
+
+<tr>
+<td class="bldg">7</td>
+<td class="unit">132 133 232 233</td>
+<td class="coord">36.80594330132077, -119.74177551162335</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4693 E Alamos Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">8</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.805809978946456, -119.74132920166261</td>
+</tr>
+
+<tr>
+<td class="bldg">8</td>
+<td class="unit">103 104 203 204</td>
+<td class="coord">36.80583578851785, -119.74150564025376</td>
+</tr>
+
+<tr>
+<td class="bldg">8</td>
+<td class="unit">105 106 205 206</td>
+<td class="coord">36.805811483898296, -119.74166698313816</td>
+</tr>
+
+<tr>
+<td class="bldg">9</td>
+<td class="unit">107 108</td>
+<td class="coord">36.80590342535318, -119.74148077608642</td>
+</tr>
+
+<tr>
+<td class="bldg">9</td>
+<td class="unit">109 110</td>
+<td class="coord">36.805899152537954, -119.74137994135577</td>
+</tr>
+
+<tr>
+<td class="bldg">9</td>
+<td class="unit">111 112</td>
+<td class="coord">36.805903190621756, -119.74127151816054</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4895 N Woodrow Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">10</td>
+<td class="unit">101 102</td>
+<td class="coord">36.80631052339002, -119.74127811526783</td>
+</tr>
+
+<tr>
+<td class="bldg">10</td>
+<td class="unit">103 104</td>
+<td class="coord">36.806311276993135, -119.74139601200652</td>
+</tr>
+
+<tr>
+<td class="bldg">10</td>
+<td class="unit">105 106</td>
+<td class="coord">36.80631361212457, -119.7415040797288</td>
+</tr>
+
+<tr>
+<td class="bldg">12</td>
+<td class="unit">107 108 207 208</td>
+<td class="coord">36.806274656280614, -119.74163520616646</td>
+</tr>
+
+<tr>
+<td class="bldg">12</td>
+<td class="unit">109 110 209 210</td>
+<td class="coord">36.806410685427686, -119.741678435731</td>
+</tr>
+
+<tr>
+<td class="bldg">12</td>
+<td class="unit">111 112 211 212</td>
+<td class="coord">36.80652080030992, -119.74168283288755</td>
+</tr>
+
+<tr>
+<td class="bldg">12</td>
+<td class="unit">113 213</td>
+<td class="coord">36.8066640186412, -119.74172394020972</td>
+</tr>
+
+<tr>
+<td class="bldg">11</td>
+<td class="unit">114 115 214 215</td>
+<td class="coord">36.806402092808014, -119.74151077008325</td>
+</tr>
+
+<tr>
+<td class="bldg">11</td>
+<td class="unit">116 117 216 217</td>
+<td class="coord">36.80637016047804, -119.74135002969068</td>
+</tr>
+
+<tr>
+<td class="bldg">15</td>
+<td class="unit">118 119 218 219</td>
+<td class="coord">36.806621640264495, -119.74122167172945</td>
+</tr>
+
+<tr>
+<td class="bldg">15</td>
+<td class="unit">120 121 220 221</td>
+<td class="coord">36.80675133717263, -119.74118506074628</td>
+</tr>
+
+<tr>
+<td class="bldg">16</td>
+<td class="unit">122 222</td>
+<td class="coord">36.80682258333502, -119.74105674558801</td>
+</tr>
+
+<tr>
+<td class="bldg">16</td>
+<td class="unit">123 124 223 224</td>
+<td class="coord">36.806691851620776, -119.74111979517185</td>
+</tr>
+
+<tr>
+<td class="bldg">16</td>
+<td class="unit">125 126 225 226</td>
+<td class="coord">36.80656761793518, -119.74115644411334</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4920 N Backer Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">13</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.806844956301966, -119.74181775356148</td>
+</tr>
+
+<tr>
+<td class="bldg">14</td>
+<td class="unit">103 104 203 204</td>
+<td class="coord">36.80684662318981, -119.74163194251702</td>
+</tr>
+
+<tr>
+<td class="bldg">14</td>
+<td class="unit">105 106 205 206</td>
+<td class="coord">36.80685451104325, -119.74146211912354</td>
+</tr>
+
+<tr>
+<td class="bldg">17</td>
+<td class="unit">107 108 207 208</td>
+<td class="coord">36.80684784188196, -119.74108401512059</td>
+</tr>
+
+<tr>
+<td class="bldg">17</td>
+<td class="unit">109 110 111 112 209 210 211 212</td>
+<td class="coord">36.80705101056787, -119.74119155129652</td>
+</tr>
+
+<tr>
+<td class="bldg">17</td>
+<td class="unit">113 114 115 116 213 214 215 216</td>
+<td class="coord">36.80723420743215, -119.74116980914071</td>
+</tr>
+
+<tr>
+<td class="bldg">19</td>
+<td class="unit">117 118 217 218</td>
+<td class="coord">36.80729188704736, -119.74162669204087</td>
+</tr>
+
+<tr>
+<td class="bldg">19</td>
+<td class="unit">119 120 121 122 219 220 221 222</td>
+<td class="coord">36.80727234633376, -119.74191515597518</td>
+</tr>
+
+<tr>
+<td class="bldg">19</td>
+<td class="unit">123 124 223 224</td>
+<td class="coord">36.80736019212167, -119.74211341404558</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4965 N Woodrow Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">18</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.807321611469604, -119.74125889313275</td>
+</tr>
+
+<tr>
+<td class="bldg">18</td>
+<td class="unit">103 104 105 106 203 204 205 206</td>
+<td class="coord">36.80752141601127, -119.7412592033884</td>
+</tr>
+
+<tr>
+<td class="bldg">18</td>
+<td class="unit">107 108 207 208</td>
+<td class="coord">36.80771676311395, -119.74133416540938</td>
+</tr>
+
+<tr>
+<td class="bldg">24</td>
+<td class="unit">109 110 209 210</td>
+<td class="coord">36.80769568410625, -119.74170933732553</td>
+</tr>
+
+<tr>
+<td class="bldg">24</td>
+<td class="unit">111 112 113 114 211 212 213 214</td>
+<td class="coord">36.80789139870592, -119.74164761375388</td>
+</tr>
+
+<tr>
+<td class="bldg">24</td>
+<td class="unit">115 116 215 216</td>
+<td class="coord">36.80807227021482, -119.74162943581109</td>
+</tr>
+
+<tr>
+<td class="bldg">22</td>
+<td class="unit">117 118 217 218</td>
+<td class="coord">36.808135725666006, -119.74187003300717</td>
+</tr>
+
+<tr>
+<td class="bldg">22</td>
+<td class="unit">119 120 121 122 219 220 221 222</td>
+<td class="coord">36.80815537735449, -119.74161616373175</td>
+</tr>
+
+<tr>
+<td class="bldg">22</td>
+<td class="unit">123 124 223 224</td>
+<td class="coord">36.80815466055685, -119.74140485246352</td>
+</tr>
+
+<tr>
+<td class="bldg">23</td>
+<td class="unit">125 126 225 226</td>
+<td class="coord">36.80814624690627, -119.74132675429667</td>
+</tr>
+
+<tr>
+<td class="bldg">23</td>
+<td class="unit">127 128 129 130 227 228 229 230</td>
+<td class="coord">36.80815243846665, -119.74110887449822</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4966 N Backer Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">20</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.807488914050815, -119.7419473858806</td>
+</tr>
+
+<tr>
+<td class="bldg">20</td>
+<td class="unit">103 104 105 106 203 204 205 206</td>
+<td class="coord">36.807668254635225, -119.74198534284191</td>
+</tr>
+
+<tr>
+<td class="bldg">20</td>
+<td class="unit">107 108 207 208</td>
+<td class="coord">36.807848543655616, -119.74198459363093</td>
+</tr>
+
+<tr>
+<td class="bldg">21</td>
+<td class="unit">109 110 209 210</td>
+<td class="coord">36.807907195151266, -119.74199134526347</td>
+</tr>
+
+<tr>
+<td class="bldg">21</td>
+<td class="unit">111 112 113 114 211 212 213 214</td>
+<td class="coord">36.80808391498018, -119.74199162102708</td>
+</tr>
+
+<tr>
+<td class="bldg">21</td>
+<td class="unit">115 116 215 216</td>
+<td class="coord">36.80826763981124, -119.74195645908505</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4969 N Backer Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">30</td>
+<td class="unit">101 102 201 202</td>
+<td class="coord">36.80749419237975, -119.74327963712194</td>
+</tr>
+
+<tr>
+<td class="bldg">30</td>
+<td class="unit">103 104 105 106 203 204 205 206</td>
+<td class="coord">36.80740603875567, -119.74353754683989</td>
+</tr>
+
+<tr>
+<td class="bldg">30</td>
+<td class="unit">107 108 109 110 207 208 209 210</td>
+<td class="coord">36.80740401825652, -119.74373619422319</td>
+</tr>
+
+<tr>
+<td class="bldg">29</td>
+<td class="unit">111 112 211 212</td>
+<td class="coord">36.80763360389109, -119.74384771942107</td>
+</tr>
+
+<tr>
+<td class="bldg">29</td>
+<td class="unit">113 114 115 116 213 214 215 216</td>
+<td class="coord">36.80781290837908, -119.74386017621818</td>
+</tr>
+
+<tr>
+<td class="bldg">29</td>
+<td class="unit">117 118 119 120 217 218 219 220</td>
+<td class="coord">36.80800329846325, -119.74382513417534</td>
+</tr>
+
+<tr>
+<td class="bldg">28</td>
+<td class="unit">121 122 123 124 221 222 223 224</td>
+<td class="coord">36.80817039361737, -119.74387175409389</td>
+</tr>
+
+<tr>
+<td class="bldg">28</td>
+<td class="unit">125 126 127 128 225 226 227 228</td>
+<td class="coord">36.808168087268974, -119.74365281246722</td>
+</tr>
+
+<tr>
+<td class="bldg">28</td>
+<td class="unit">129 130 229 230</td>
+<td class="coord">36.80821419833679, -119.74342509079857</td>
+</tr>
+
+<tr>
+<td class="bldg">27</td>
+<td class="unit">131 132 231 232</td>
+<td class="coord">36.808144206517944, -119.74335474446232</td>
+</tr>
+
+<tr>
+<td class="bldg">27</td>
+<td class="unit">133 134 135 136 233 234 235 236</td>
+<td class="coord">36.808160048366, -119.74311129184179</td>
+</tr>
+
+<tr>
+<td class="bldg">27</td>
+<td class="unit">137 138 237 238</td>
+<td class="coord">36.808208294543846, -119.74286736305359</td>
+</tr>
+
+<tr>
+<td class="bldg">26</td>
+<td class="unit">139 140 239 240</td>
+<td class="coord">36.808251153548525, -119.74277884352985</td>
+</tr>
+
+<tr>
+<td class="bldg">26</td>
+<td class="unit">141 142 143 144 241 242 243 244</td>
+<td class="coord">36.80804930622967, -119.74276389566639</td>
+</tr>
+
+<tr>
+<td class="bldg">26</td>
+<td class="unit">145 146 245 246</td>
+<td class="coord">36.80786708942736, -119.74274311241743</td>
+</tr>
+
+<tr>
+<td class="bldg">25</td>
+<td class="unit">147 148 247 248</td>
+<td class="coord">36.80781296143765, -119.7427488660848</td>
+</tr>
+
+<tr>
+<td class="bldg">25</td>
+<td class="unit">149 150 151 152 249 250 251 252</td>
+<td class="coord">36.80763554257965, -119.74275742728874</td>
+</tr>
+
+<tr>
+<td class="bldg">25</td>
+<td class="unit">153 154 253 254</td>
+<td class="coord">36.80745090323788, -119.7427233537483</td>
+</tr>
+
+<tr>
+<td class="bldg">31</td>
+<td class="unit">155 156 255 256</td>
+<td class="coord">36.80790340151509, -119.74282692085089</td>
+</tr>
+
+<tr>
+<td class="bldg">31</td>
+<td class="unit">157 158 159 160 257 258 259 260</td>
+<td class="coord">36.807860105063654, -119.7430707600932</td>
+</tr>
+
+<tr>
+<td class="bldg">31</td>
+<td class="unit">161 162 163 164 261 262 263 264</td>
+<td class="coord">36.80788475575317, -119.74329274297703</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+<li class="mapp-table">
+<span class="mapp-address">4909 N Backer Ave</span>
+
+<table>
+<thead>
+<tr>
+<th class="bldg">bldg #</th>
+<th class="unit">unit #</th>
+<th class="coord">coordinate</th>
+</tr>
+</thead>
+<tbody>
+
+<tr>
+<td class="bldg">34</td>
+<td class="unit">101 102 103 104 201 202 203 204</td>
+<td class="coord">36.80738368914438, -119.74449329582121</td>
+</tr>
+
+<tr>
+<td class="bldg">34</td>
+<td class="unit">105 106 107 108 205 206 207 208</td>
+<td class="coord">36.807390792271065, -119.74468964531569</td>
+</tr>
+
+<tr>
+<td class="bldg">34</td>
+<td class="unit">109 110 209 210</td>
+<td class="coord">36.80748525867986, -119.7449635744952</td>
+</tr>
+
+<tr>
+<td class="bldg">33</td>
+<td class="unit">111 112 113 114 211 212 213 214</td>
+<td class="coord">36.80789923366865, -119.7447695673157</td>
+</tr>
+
+<tr>
+<td class="bldg">33</td>
+<td class="unit">115 116 117 118 215 216 217</td>
+<td class="coord">36.80809681302778, -119.74481442806788</td>
+</tr>
+
+<tr>
+<td class="bldg">33</td>
+<td class="unit">119 120 219 220</td>
+<td class="coord">36.80826653147313, -119.74483427378253</td>
+</tr>
+
+<tr>
+<td class="bldg">32</td>
+<td class="unit">121 122 221 222</td>
+<td class="coord">36.80821293457678, -119.74471650547882</td>
+</tr>
+
+<tr>
+<td class="bldg">32</td>
+<td class="unit">123 124 125 126 223 224 225 226</td>
+<td class="coord">36.80814541141745, -119.74448565694938</td>
+</tr>
+
+<tr>
+<td class="bldg">32</td>
+<td class="unit">127 128 129 130 227 228 229 230</td>
+<td class="coord">36.80816953530787, -119.74424293240374</td>
+</tr>
+
+<tr>
+<td class="bldg">35</td>
+<td class="unit">131 132 133 134 231 232 233 234</td>
+<td class="coord">36.8079861734863, -119.74438658145996</td>
+</tr>
+
+<tr>
+<td class="bldg">35</td>
+<td class="unit">135 136 137 138 235 236 237 238</td>
+<td class="coord">36.80780387478679, -119.74441694920019</td>
+</tr>
+
+<tr>
+<td class="bldg">35</td>
+<td class="unit">139 140 239 240</td>
+<td class="coord">36.80762071367466, -119.74448665372459</td>
+</tr>
+
+</tbody>
+</table>
+</li>
+
+</ul>
+</div>
